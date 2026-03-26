4ed479205a63d6d33be81f863eda081d478af624
Samuel Stanley <148486731+samuelstanley0101@users.noreply.github.com>
2026-03-26 18:28:18 -0400

    changed script path

--- radio-receiver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# RADIO RECEIVER SCRIPT
-# This is a script to start the radio receiver program on startup.
-# Systemd will execute any commands in this script after the system boots.
-
-ping google.com > /home/rocketry/ping-test

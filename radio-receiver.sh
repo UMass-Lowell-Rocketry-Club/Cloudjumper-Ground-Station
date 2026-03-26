ffb171fcf5c77116bbe85de01018da68de0c1e30
Samuel Stanley <148486731+samuelstanley0101@users.noreply.github.com>
2026-03-26 18:32:04 -0400

    created basic script

---
+++ radio-receiver.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# RADIO RECEIVER SCRIPT
+# This is a script to start the radio receiver program on startup.
+# Systemd will execute any commands in this script after the system boots.
+
+while true; do
+  sleep 10
+done
